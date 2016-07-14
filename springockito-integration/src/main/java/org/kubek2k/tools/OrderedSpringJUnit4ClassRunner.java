411e441e0bbf35310015a4302a89e0e7a27a8a03
Oliver von Behr Kuster <olivervbk@gmail.com>
2016-07-14 15:18:59 -0300

    Springockito-integration clean up

--- springockito-integration/src/main/java/org/kubek2k/tools/OrderedSpringJUnit4ClassRunner.java
+++ springockito-integration/src/main/java/org/kubek2k/tools/OrderedSpringJUnit4ClassRunner.java
@@ -1,39 +1,50 @@
 package org.kubek2k.tools;
 
-import org.junit.runners.model.FrameworkMethod;
-import org.junit.runners.model.InitializationError;
-
 import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class OrderedSpringJUnit4ClassRunner extends org.springframework.test.context.junit4.SpringJUnit4ClassRunner {
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
 
-    public OrderedSpringJUnit4ClassRunner(Class<?> clazz) throws InitializationError {
-        super(clazz);
-    }
+public class OrderedSpringJUnit4ClassRunner
+	extends org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+{
 
-    @Override
-    protected List<FrameworkMethod> computeTestMethods() {
-        List<FrameworkMethod> frameworkMethods = super.computeTestMethods();
-        Collections.sort(frameworkMethods, new Comparator<FrameworkMethod>() {
-            @Override
-            public int compare(FrameworkMethod o1, FrameworkMethod o2) {
-                Integer i1 = getOrder(o1);
-                Integer i2 = getOrder(o2);
-                return i1.compareTo(i2);
-            }
-        });
-        return frameworkMethods;
-    }
+	public OrderedSpringJUnit4ClassRunner( final Class< ? > clazz )
+		throws InitializationError
+	{
+		super( clazz );
+	}
 
-    private int getOrder(FrameworkMethod frameworkMethod) {
-        Method method = frameworkMethod.getMethod();
-        if (method.isAnnotationPresent(Ordered.class)) {
-            return method.getAnnotation(Ordered.class).value();
-        } else {
-            return Ordered.DEFAULT;
-        }
-    }
+	@Override
+	protected List<FrameworkMethod> computeTestMethods()
+	{
+		final List<FrameworkMethod> frameworkMethods = super.computeTestMethods();
+		Collections.sort( frameworkMethods, new Comparator<FrameworkMethod>()
+		{
+
+			public int compare( final FrameworkMethod o1, final FrameworkMethod o2 )
+			{
+				final Integer i1 = getOrder( o1 );
+				final Integer i2 = getOrder( o2 );
+				return i1.compareTo( i2 );
+			}
+		} );
+		return frameworkMethods;
+	}
+
+	private int getOrder( final FrameworkMethod frameworkMethod )
+	{
+		final Method method = frameworkMethod.getMethod();
+		if ( method.isAnnotationPresent( Ordered.class ) )
+		{
+			return method.getAnnotation( Ordered.class ).value();
+		}
+		else
+		{
+			return Ordered.DEFAULT;
+		}
+	}
 }
